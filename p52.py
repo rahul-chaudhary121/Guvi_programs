fee93f474e252903a205d5984b5d09d90185c4a8
rahul-chaudhary121 <44108236+rahul-chaudhary121@users.noreply.github.com>
2019-03-17 03:23:30 -0700

    Update p52.py

--- p52.py
+++ p52.py
@@ -1,3 +1,8 @@
 l=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
-n=int(input())
-print(l[n])
+l1=['0','1','2','3','4','5','6','7','8','9','10']
+n=input()
+if in l1: 
+  n=int(n)
+  print(l[n])
+ else:
+  print("Invalid Input")
